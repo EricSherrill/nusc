648b775be5c249b1970c12faeac9453e8d0187e3
jpl1079 <jpl1079@gmail.com>
2015-05-07 16:47:48 -0400

    initial commit after do-over

---
+++ arrays.js
@@ -0,0 +1,53 @@
+// Taken from TR-3838
+var drives = [
+{"model_num":"X410A-R5","mkt_size":300,"type":"SAS","rpm_speed":10000,"encrypted":"","checksum":"BCS","approx_iops":140,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":16,"rg_size_dp_max":28,"physical_size_mib":280104,"right_size_mib":272000},
+{"model_num":"X421A-R5","mkt_size":450,"type":"SAS","rpm_speed":10000,"encrypted":"","checksum":"BCS","approx_iops":140,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":16,"rg_size_dp_max":28,"physical_size_mib":420156,"right_size_mib":418000},
+{"model_num":"X422A-R5","mkt_size":600,"type":"SAS","rpm_speed":10000,"encrypted":"","checksum":"BCS","approx_iops":140,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":16,"rg_size_dp_max":28,"physical_size_mib":560208,"right_size_mib":560000},
+{"model_num":"X423A-R5","mkt_size":900,"type":"SAS","rpm_speed":10000,"encrypted":"","checksum":"BCS","approx_iops":140,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":16,"rg_size_dp_max":28,"physical_size_mib":858483,"right_size_mib":857000},
+{"model_num":"X425A-R6","mkt_size":1200,"type":"SAS","rpm_speed":10000,"encrypted":"","checksum":"BCS","approx_iops":140,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":16,"rg_size_dp_max":28,"physical_size_mib":1144641,"right_size_mib":1142352},
+{"model_num":"X416A-R5","mkt_size":600,"type":"SAS","rpm_speed":10000,"encrypted":true,"checksum":"BCS","approx_iops":140,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":16,"rg_size_dp_max":28,"physical_size_mib":560208,"right_size_mib":560000},
+{"model_num":"X417A-R6","mkt_size":900,"type":"SAS","rpm_speed":10000,"encrypted":true,"checksum":"BCS","approx_iops":140,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":16,"rg_size_dp_max":28,"physical_size_mib":858483,"right_size_mib":857000},
+{"model_num":"X441A-R5","mkt_size":100,"type":"SSD","rpm_speed":null,"encrypted":"","checksum":"BCS","approx_iops":6000,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":23,"rg_size_dp_max":28,"physical_size_mib":95396,"right_size_mib":95146},
+{"model_num":"X442A-R5","mkt_size":100,"type":"SSD","rpm_speed":null,"encrypted":"","checksum":"BCS","approx_iops":6000,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":23,"rg_size_dp_max":28,"physical_size_mib":84796,"right_size_mib":84574},
+{"model_num":"X446B-R6","mkt_size":200,"type":"SSD","rpm_speed":null,"encrypted":"","checksum":"BCS","approx_iops":6000,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":23,"rg_size_dp_max":28,"physical_size_mib":190782,"right_size_mib":190532},
+{"model_num":"X438A-R6","mkt_size":400,"type":"SSD","rpm_speed":null,"encrypted":"","checksum":"BCS","approx_iops":6000,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":23,"rg_size_dp_max":28,"physical_size_mib":381554,"right_size_mib":381304},
+{"model_num":"X447A-R6","mkt_size":800,"type":"SSD","rpm_speed":null,"encrypted":"","checksum":"BCS","approx_iops":6000,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":23,"rg_size_dp_max":28,"physical_size_mib":763097,"right_size_mib":762847},
+{"model_num":"X440A-R6","mkt_size":800,"type":"SSD","rpm_speed":null,"encrypted":true,"checksum":"BCS","approx_iops":6000,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":23,"rg_size_dp_max":28,"physical_size_mib":763097,"right_size_mib":762847},
+{"model_num":"X439A-R6","mkt_size":1600,"type":"SSD","rpm_speed":null,"encrypted":"","checksum":"BCS","approx_iops":6000,"rg_size_4_default":8,"rg_size_4_max":14,"rg_size_dp_default":23,"rg_size_dp_max":28,"physical_size_mib":1526185,"right_size_mib":1525935},
+{"model_num":"X302A-R5","mkt_size":1000,"type":"SATA","rpm_speed":7200,"encrypted":"","checksum":"BCS","approx_iops":75,"rg_size_4_default":7,"rg_size_4_max":7,"rg_size_dp_default":14,"rg_size_dp_max":20,"physical_size_mib":847884,"right_size_mib":847555},
+{"model_num":"X306A-R5","mkt_size":2000,"type":"SATA","rpm_speed":7200,"encrypted":"","checksum":"BCS","approx_iops":75,"rg_size_4_default":7,"rg_size_4_max":7,"rg_size_dp_default":14,"rg_size_dp_max":20,"physical_size_mib":1695702,"right_size_mib":1695466},
+{"model_num":"X308A-R5","mkt_size":3000,"type":"SATA","rpm_speed":7200,"encrypted":"","checksum":"BCS","approx_iops":75,"rg_size_4_default":7,"rg_size_4_max":7,"rg_size_dp_default":14,"rg_size_dp_max":20,"physical_size_mib":2543634,"right_size_mib":2538546},
+{"model_num":"X309A-R6","mkt_size":3000,"type":"NL-SAS","rpm_speed":7200,"encrypted":true,"checksum":"BCS","approx_iops":75,"rg_size_4_default":7,"rg_size_4_max":7,"rg_size_dp_default":14,"rg_size_dp_max":20,"physical_size_mib":2861588,"right_size_mib":2855865},
+{"model_num":"X315A-R6","mkt_size":4000,"type":"NL-SAS","rpm_speed":7200,"encrypted":true,"checksum":"BCS","approx_iops":75,"rg_size_4_default":7,"rg_size_4_max":7,"rg_size_dp_default":14,"rg_size_dp_max":20,"physical_size_mib":3815447,"right_size_mib":3807816},
+{"model_num":"X477A-R6","mkt_size":4000,"type":"NL-SAS","rpm_speed":7200,"encrypted":"","checksum":"BCS","approx_iops":75,"rg_size_4_default":7,"rg_size_4_max":7,"rg_size_dp_default":14,"rg_size_dp_max":20,"physical_size_mib":3815447,"right_size_mib":3807816},
+{"model_num":"X316A-R6","mkt_size":6000,"type":"NL-SAS","rpm_speed":7200,"encrypted":"","checksum":"BCS","approx_iops":75,"rg_size_4_default":7,"rg_size_4_max":7,"rg_size_dp_default":14,"rg_size_dp_max":14,"physical_size_mib":5625872,"right_size_mib":5614621},
+{"model_num":"X478A-R5","mkt_size":3000,"type":"MSATA","rpm_speed":7200,"encrypted":"","checksum":"AZCS","approx_iops":75,"rg_size_4_default":7,"rg_size_4_max":7,"rg_size_dp_default":14,"rg_size_dp_max":20,"physical_size_mib":2891588,"right_size_mib":2811241},
+{"model_num":"X480A-R5","mkt_size":4000,"type":"MSATA","rpm_speed":7200,"encrypted":"","checksum":"AZCS","approx_iops":75,"rg_size_4_default":7,"rg_size_4_max":7,"rg_size_dp_default":14,"rg_size_dp_max":20,"physical_size_mib":3815447,"right_size_mib":3748319},
+{"model_num":"X481A-R6","mkt_size":6000,"type":"MSATA","rpm_speed":7200,"encrypted":"","checksum":"BCS","approx_iops":75,"rg_size_4_default":7,"rg_size_4_max":7,"rg_size_dp_default":14,"rg_size_dp_max":14,"physical_size_mib":5723166,"right_size_mib":5622471}
+];
+
+var outputUnits = [
+{"name": "kb", "base": 10, "displayName": "kB", "si": "kB", "iec": "KiB"},
+{"name": "mb", "base": 10, "displayName": "MB", "si": "MB", "iec": "MiB"},
+{"name": "gb", "base": 10, "displayName": "GB", "si": "GB", "iec": "GiB"},
+{"name": "tb", "base": 10, "displayName": "TB", "si": "TB", "iec": "TiB"},
+{"name": "pb", "base": 10, "displayName": "PB", "si": "PB", "iec": "PiB"},
+{"name": "eb", "base": 10, "displayName": "EB", "si": "EB", "iec": "EiB"},
+{"name": "zb", "base": 10, "displayName": "ZB", "si": "ZB", "iec": "ZiB"},
+{"name": "yb", "base": 10, "displayName": "YB", "si": "YB", "iec": "YiB"},
+{"name": "Auto", "base": 2, "displayName": "Auto", "si": "", "iec": ""},
+{"name": "bytes", "base": 2, "displayName": "bytes", "si": "bytes", "iec": "bytes"},
+{"name": "kib", "base": 2, "displayName": "KiB", "si": "kB", "iec": "KiB"},
+{"name": "mib", "base": 2, "displayName": "MiB", "si": "MB", "iec": "MiB"},
+{"name": "gib", "base": 2, "displayName": "GiB", "si": "GB", "iec": "GiB"},
+{"name": "tib", "base": 2, "displayName": "TiB", "si": "TB", "iec": "TiB"},
+{"name": "pib", "base": 2, "displayName": "PiB", "si": "PB", "iec": "PiB"},
+{"name": "eib", "base": 2, "displayName": "EiB", "si": "EB", "iec": "EiB"},
+{"name": "zib", "base": 2, "displayName": "ZiB", "si": "ZB", "iec": "ZiB"},
+{"name": "yib", "base": 2, "displayName": "YiB", "si": "YB", "iec": "YiB"}
+];
+
+var raidTypes = [
+{"displayName": "RAID-DP", "name": "raidDP", "parity_drives": 2},
+{"displayName": "RAID-4", "name": "raid4", "parity_drives": 1}
+];
---
+++ main.js
@@ -0,0 +1,136 @@
+(function() {
+
+  var app = angular.module('app', [])
+
+  .controller('MainController', function($scope) {
+
+    // import from drives.js
+    $scope.drives = drives;
+    $scope.outputUnits = outputUnits;
+    $scope.raidTypes = raidTypes;
+
+    // drive_table.html settings
+    $scope.sortType = "-mkt_size";
+    $scope.sortReverse = true;
+
+    // set some defaults for calculator
+    $scope.totalDrives = 24;
+    $scope.spareDrives = 1;
+    $scope.rootDrives = 0;
+    $scope.selectedDrive = $scope.drives[14];
+    $scope.selectedRaidType = $scope.raidTypes[0];
+    $scope.selectedOutputUnit = $scope.outputUnits[8].iec;
+    $scope.snapReserve = 0;
+    $scope.waflReserve = 10;
+    $scope.aggrReserve = 0;
+    $scope.rgSize = $scope.selectedDrive.rg_size_dp_default;
+    // number of aggregates (future use)
+
+
+    // calculated properties
+    $scope.usableDrives = function() {
+      return $scope.totalDrives - ($scope.spareDrives + $scope.rootDrives);
+    };
+
+    $scope.numRaidGroups = function () {
+      return Math.ceil($scope.usableDrives() / $scope.rgSize);
+    };
+
+    $scope.parityDrives = function() {
+      return $scope.numRaidGroups() * $scope.selectedRaidType.parity_drives;
+    };
+
+    $scope.dataDrives = function() {
+      return $scope.usableDrives() - $scope.parityDrives();
+    };
+
+    $scope.totalDriveEfficiency = function() {
+      return ($scope.dataDrives() / $scope.totalDrives) * 100;
+    };
+
+    $scope.usableDriveEfficiency = function() {
+      return ($scope.dataDrives() / $scope.usableDrives()) * 100;
+    };
+
+    $scope.totalRawCapacity = function () {
+      return $scope.totalDrives * $scope.selectedDrive.mkt_size;
+    };
+
+    $scope.totalPhysicalCapacity = function () {
+      return $scope.totalDrives * $scope.selectedDrive.physical_size_mib;
+    };
+
+    $scope.totalRightSizeCapacity = function () {
+      return $scope.totalDrives * $scope.selectedDrive.right_size_mib;
+    };
+
+    $scope.aggrCapacity = function () {
+      return $scope.dataDrives() * $scope.selectedDrive.right_size_mib;
+    };
+    // TR-3838, page 9
+    $scope.volumeCapacity = function () {
+      return ($scope.aggrCapacity() * 0.9) * 0.995;
+    };
+
+  }); // close controller
+
+  // http://stackoverflow.com/questions/18381944/ng-options-and-unique-filter-not-displaying-angular-js
+  app.filter('unique', function() {
+    return function(input, key) {
+      var unique = {};
+      var uniqueList = [];
+      for (var i = 0; i < input.length; i++) {
+        if (typeof unique[input[i][key]] == "undefined") {
+          unique[input[i][key]] = "";
+          uniqueList.push(input[i]);
+        }
+      }
+      return uniqueList;
+    };
+  });
+
+  /* filters mostly derived from https://gist.github.com/thomseddon/3511330 &
+  http://stackoverflow.com/a/25651291/4793917 */
+
+  app.filter('base2orig', function() {
+    return function(bytes, precision, inputUnit) {
+      if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+      if (typeof precision === 'undefined') precision = 2;
+      var units = ['bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'],
+        number = Math.floor(Math.log(bytes) / Math.log(1024)),
+        offset = (units.indexOf(inputUnit));
+      return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number + offset];
+    };
+  });
+
+  // modified to add ability to specify input and output units
+  app.filter('base2convert', function() {
+    return function(input, precision, inUnit, outUnit) {
+      if (isNaN(parseFloat(input)) || !isFinite(input)) return '-';
+      if (typeof precision === 'undefined') precision = 2;
+      var units = ['bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+      var exp = Math.floor(Math.log(input) / Math.log(1024));
+      var offset = (typeof inUnit === 'undefined') ? 0 : units.indexOf(inUnit);
+      if (typeof outUnit === 'undefined' || outUnit === '' || outUnit === null) {
+        return (input / Math.pow(1024, Math.floor(exp))).toFixed(precision) + ' ' + units[exp + offset];
+      } else {
+        exp = (units.indexOf(outUnit) - units.indexOf(inUnit));
+        return (input / Math.pow(1024, Math.floor(exp))).toFixed(precision) + ' ' + units[units.indexOf(outUnit)];
+      }
+    }
+  });
+
+  app.filter('base10convert', function() {
+    return function(bytes, precision, inputUnit) {
+      if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+      if (typeof precision === 'undefined') precision = 2;
+      var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        number = Math.floor(Math.log(bytes) / Math.log(1000)),
+        offset = (units.indexOf(inputUnit));
+      return (bytes / Math.pow(1000, Math.floor(number))).toFixed(precision) + ' ' + units[number + offset];
+    };
+  });
+
+
+
+}()); // close IIFE
